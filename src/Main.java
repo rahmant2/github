d2be34c5f8e948047be7599500abf1daca11bc13
BT_4N2_14 <rahmant2@nycstudents.net>
2023-09-19 13:04:59 -0400

    Added a line for my favorite food

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Name: Rahman");
         System.out.println("Movie: Wolf of Wall Street");
+        System.out.println("Favorite Food: Chicken");
+
 
 
     }
